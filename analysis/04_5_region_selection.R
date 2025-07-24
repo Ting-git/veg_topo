06d2fc76ea3fc0ee9884b0619c117921f22a86bc
Ting Tan <tanting0502@163.com>
2025-07-24 17:40:38 +0200

    Initial commit

---
+++ analysis/04_5_region_selection.R
@@ -0,0 +1,105 @@
+
+
+
+# ------------ Set Up ----------------------------------------------------------
+library(terra)
+library(tidyr)
+library(dplyr)
+library(ggplot2)
+library(patchwork)
+library(DataExplorer)
+library(RColorBrewer)
+
+# Load configuration and functions
+source(here::here("config.R"))
+
+
+# ------------ Data Pre for whole-----------------------------------------------
+
+# Load resampled raster datasets (AI, TWI, fused)
+
+ai_55km_r <- terra::rast(ai_55km_file) * 0.0001 # Multiply all values by 0.0001 to get original value
+fused_55km_r <- terra::rast(fused_55km_file)
+dem_sd_55km_r <- terra::rast(dem_sd_55km_path)
+fpa_55km_r <- terra::rast(fpa_55km_path)
+
+# Stack rasters into a single SpatRaster
+stacked <- c(ai_55km_r,
+             fused_55km_r,
+             dem_55km_r,
+             fpa_55km_r)
+
+# Convert to data frame
+df <- as.data.frame(stacked, xy = TRUE, na.rm = TRUE)
+colnames(df) <- c("lon", "lat", "ai", "fused", "relief", "fpa")
+
+# Clean up
+rm(ai_55km_r, fused_55km_r, dem_55km_r, fpa_55km_r)
+gc()
+
+
+
+# ----------- Overview: plot the histogram for all variables  -------------------
+
+# Ploting
+p_hg <- DataExplorer::plot_histogram(df)
+
+# combine the plots
+cp_hg <- patchwork::wrap_plots(p_hg)
+
+# Save histogram plot
+ggsave(
+  filename = here::here("data/figures/04_variables_hg.png"),
+  plot = cp_hg,
+  width = 10,
+  height = 5,
+  dpi = 300,
+  units = "in"
+)
+
+
+# ------------ region -----------------------------------------------
+
+
+df_binned <- df |>
+
+  # create bins
+  mutate(
+    ai_bin = cut(ai, breaks = quantile(ai, probs = seq(0, 1, 1/5)), include.lowest = TRUE),
+    relief_bin = cut(relief, breaks = quantile(relief, probs = seq(0, 1, 1/3)), include.lowest = TRUE),
+    lat_bin = cut(lat, breaks = quantile(lat, probs = seq(0, 1, 1/5)), include.lowest = TRUE)
+  ) |>
+
+  # use only data where influence of humans on veg height is minimal
+  filter(fused < 0.05, fpa = 1) |>
+
+  # Filter to retain only data that is in bins 1, 3, and 5 for MI and Lat.
+  # and for bins 1 and 3 for relief
+  filter(
+    ai_bin %in% c(...),  # retain only data that is in bins 1, 3, and 5
+    relief_bin %in% c(...),  # retain only data that is in bins 1 and 3
+    lat_bin %in% c(... )  # retain only data that is in bins 1, 3, and 5
+  )
+
+# Fig. 6 Down Valley drainage
+mutate(strata_A = interaction(ai_bin, relief_bin, drop = TRUE))
+
+# Fig. 6 Aspect difference
+mutate(strata_B = interaction(lat_bin, relief_bin, drop = TRUE))
+
+# pick one region (coarse gridcell) representing all combination of bins.
+# Should yield 6 samples
+
+set.seed(999)
+
+# for Fig. 6 Down Valley drainage
+df_samples_A <- df_binned |>
+  group_by(strata_A) |>
+  sample_n(size = 1, replace = FALSE) |>
+  ungroup()
+
+# for Fig. 6 Aspect difference
+df_samples_B <- df_binned |>
+  group_by(strata_B) |>
+  sample_n(size = 1, replace = FALSE) |>
+  ungroup()
