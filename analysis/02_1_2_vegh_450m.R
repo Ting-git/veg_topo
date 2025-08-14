b0e805c859d9c408163f0111f85dccb6c77755f4
Ting Tan <tanting0502@163.com>
2025-08-14 13:57:33 +0200

    Rename and delete the storage of intermediate files and prepare for subsequent optimization, such as resampling in advance

--- analysis/02_1_2_vegh_450m.R
+++ analysis/02_1_2_vegh_450m.R
@@ -1,5 +1,5 @@
-# ------Set up------------------------------------------------------------------------
-
+# ------Set up------------------------------------------------------------------
+# Load packages
 library(terra)
 library(furrr)
 library(stringr)
@@ -7,24 +7,25 @@ library(stringr)
 source(here::here("config.R"))
 source(here::here("R/aggregate_byfile.R"))
 
-# -------Configuration-----------------------------------------------------------------------
+# -------Configuration----------------------------------------------------------
 
 vegh_10m_tiles_path <- fs::dir_ls(path = vegh_10m_tiles_dir, glob = "*_Map.tif")
 # vegh_10m_tiles_path_sub <- vegh_10m_tiles_path[1:10]
 
-# -----Aggregation: workers=8, 97 min--------------------------------------------
-# vegh 10m --> vegh 450m
+vegh_450m_tiles_dir <- file.path(veg_topo_extr_dir, "data/global_vegh_450m/3_3_deg")
+if (!dir.exists(vegh_450m_tiles_dir)) {
+  dir.create(vegh_450m_tiles_dir, recursive = TRUE)
+}
 
-# get the resolution information of target raster(ga2 TWI)
-rast_tar <- rast(twi_450m_mosaic_clean_path)
-xres_tar <- xres(rast_tar)
-yres_tar <- yres(rast_tar)
+# -----Aggregation: 10m -> 450m: workers=8, 97 min------------------------------
 
-rm(rast_tar)
-gc()
+# target layer
+twi_450m_r <- terra::rast(twi_450m_mosaic_clean_path)
+twi_450m_r
+xres_tar <- res(twi_450m_r)[1]
+yres_tar <- res(twi_450m_r)[2]
 
 # Set up parallel processing (adjust based on available CPU cores)
-
 plan(multisession, workers = 8)
 t0 <- Sys.time()
 
@@ -43,7 +44,8 @@ results <- future_map(vegh_10m_tiles_path, function(file) {
     # Run the main aggregation function
     result <- aggregate_byfile(input_path = file,
                                output_path = output_path,
-                               target_path =  twi_450m_mosaic_clean_path,
+                               xres_tar <- xres_tar,
+                               yres_tar <- yres_tar,
                                if_resample = FALSE)
 
     return(list(success = TRUE, out_file = result, error = NULL))
@@ -65,18 +67,23 @@ gc()
 message(sprintf("done [%.1fs]", difftime(Sys.time(), t0, units = "secs")))
 saveRDS(results, file = here::here("data/results_vegh_aggregate.rds"))
 
-# ------------Combination------10 min-------------------------------------------------
+# ------------Combination------15 min-------------------------------------------------
 
 # Load rasters and merge into a mosaic
 vegh_450m_tiles_path <- fs::dir_ls(path = vegh_450m_tiles_dir, glob = "*_to450m.nc")
 vegh_450m_tiles_r <- lapply(vegh_450m_tiles_path, terra::rast)
-
 vegh_450m_mosaic_r <- do.call(terra::merge, unname(vegh_450m_tiles_r))
 
+# Resample using TWI data as target
+vegh_450m_mosaic_r <- terra::rast(vegh_450m_mosaic_path)
+vegh_450m_mosaic_rr <- terra::resample(vegh_450m_mosaic_r, twi_450m_r, method = "bilinear")
+vegh_450m_mosaic_rr
+
 # Save merged raster
-terra::writeCDF(vegh_450m_mosaic_r, vegh_450m_mosaic_path, overwrite = TRUE)
+terra::writeCDF(vegh_450m_mosaic_rr, vegh_450m_mosaic_path, overwrite = TRUE)
 message("✅ Saved successfully to: ", vegh_450m_mosaic_path)
 
-rm(vegh_450m_mosaic_r, vegh_450m_tiles_r)
-gc()
 
+# ---------- Delete intermediate data ------------------------------------------
+files <- list.files(vegh_450m_tiles_dir, full.names = TRUE, recursive = TRUE)
+if (length(files) > 0) file.remove(files)
