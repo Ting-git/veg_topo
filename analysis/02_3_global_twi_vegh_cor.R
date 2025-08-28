e0c7b0b04b86556021d34a747a0455bb7f26ed63
Ting Tan <tanting0502@163.com>
2025-08-28 13:28:35 +0200

    Reorganize data processing: include TWI and VEGH handling in parallel correlation, skip per-tile saving

--- analysis/02_3_global_twi_vegh_cor.R
+++ analysis/02_3_global_twi_vegh_cor.R
@@ -1,24 +1,24 @@
 # done [25.1 min]
 
-# ------Load required libraries-------------------------------------------------------------
+# ------Load required libraries-------------------------------------------------
 
 library(terra)     # For handling raster data
 library(furrr)
 library(dplyr)
 
-# ------Load configuration and helper functions---------------------------------------------
+# ------Load configuration and helper functions---------------------------------
 
 source(here::here("config.R"))
 source(here::here("R/create_spatial_windows.R"))
-source(here::here("R/calculate_window_correlations.R"))
-source(here::here("R/mosaicing.R"))
+source(here::here("R/calculate_correlation_bywin.R"))
+source(here::here("R/mosaic_tiles.R"))
 
 # ------Analysis---------------------------------------------
 
 # read the information of valid tiles
 tiles_info <- readRDS(valid_tiles_info_path)
 
-# ------------parallel process for each tiles--------------------------------------------------
+# ------------parallel process for each tiles-----------------------------------
 
 # Clear memory and set up parallel processing
 gc()
@@ -32,43 +32,50 @@ results <- future_pmap(
     args <- list(...)
     tryCatch({
       tile_id <- args$tile_id
-      premerg_file <- args$premerg_file
+      tile_extent <- terra::ext(args$xmin, args$xmax, args$ymin, args$ymax)
 
-      message("Processing tile: ", tile_id)
+      # -------Data Pre------------
+      # Load rasters
+      twi_r <- rast(twi_450m_mosaic_clean_path)
+      vegh_r <- rast(vegh_450m_mosaic_path)
 
-      # -------Load pre-processed raster------------
-      premerg_r <- terra::rast(premerg_file)
+      # Crop to tile
+      twi_rc <- crop(twi_r, tile_extent)
+      vegh_rc <- crop(vegh_r, tile_extent)
 
-      # df_cor <- windows_cor_analysis(premerg_r)
-      df_cor  <- create_spatial_windows(premerg_r) |>
-        calculate_window_correlations()
+      # Stack and name layers
+      stacked <- c(twi_rc, vegh_rc)
+      names(stacked) <- c("twi", "vegh")
 
-      df_data <- df_cor |>
-        tidyr::unnest(cols = c(data)) |>  # Explicit cols parameter
-        ungroup()
+      # Clean up temporary rasters
+      rm(twi_r, vegh_r, twi_rc, vegh_rc)
+      gc()
 
+      # Create windows and calculate correlation
+      df_cor  <- create_spatial_windows(stacked) |>
+        calculate_correlation_bywin()
+
+      colnames(df_cor)
       # ------5km output-------
-      cor_r <- terra::rast(
-        df_cor[, c("lon_mid", "lat_mid", "correlation", "cor_pval")],
-        type = "xyz",
-        crs = "EPSG:4326"
-      )
-      names(cor_r) <- c("correlation", "cor_pval")
-      nc_path <- file.path(cor_twi_vegh_tiles_dir, paste0("cor_twi_vegh_5km_", tile_id, ".nc"))
-      terra::writeCDF(cor_r, nc_path, overwrite = TRUE)
+      cor_r <- terra::rast(df_cor[, c("lon_mid", "lat_mid", "correlation")], type = "xyz", crs = "EPSG:4326")
+      cor_nc_path <- file.path(cor_twi_vegh_tiles_dir,
+                               paste0("r_H_TWI_5km_", tile_id, "_map.nc"))
+      terra::writeCDF(cor_r, cor_nc_path, varnames = "r_H_TWI", overwrite = TRUE)
+      if(file.exists(cor_nc_path)) message(paste0("Saved:", cor_nc_path))
 
-      # ------450m output-------
-      cor_r_2 <- terra::rast(
-        df_data[, c("lon", "lat", "lon_mid", "lat_mid", "twi", "vegh", "n_obs", "correlation", "cor_pval")],
-        type = "xyz",
-        crs = "EPSG:4326"
-      )
-      names(cor_r_2) <- c("lon_mid", "lat_mid", "twi", "vegh", "n_obs", "correlation", "cor_pval")
+      pval_r <- terra::rast(df_cor[, c("lon_mid", "lat_mid", "cor_pval")], type = "xyz", crs = "EPSG:4326")
+      pval_nc_path <- file.path(cor_twi_vegh_tiles_dir,
+                                paste0("r_H_TWI_5km_", tile_id, "_pval.nc"))
+      terra::writeCDF(pval_r, pval_nc_path, varnames = "r_H_TWI_pval", overwrite = TRUE)
+      if(file.exists(pval_nc_path)) message(paste0("Saved:", pval_nc_path))
 
-      nc_path2 <- file.path(cor_twi_vegh_tiles_dir, "cor_450m", paste0("cor_twi_vegh_450m_", tile_id, ".nc"))
-      terra::writeCDF(cor_r_2, nc_path2, overwrite = TRUE)
+      # peak_r <- terra::rast(df_cor[, c("lon_mid", "lat_mid", "peak")], type = "xyz", crs = "EPSG:4326")
+      # nc_path <- file.path(cor_twi_vegh_tiles_dir,
+      #                      paste0("r_H_TWI_5km_", tile_id, "_peak.nc"))
+      # terra::writeCDF(pval_r, nc_path, varnames = "r_H_TWI_peak", overwrite = TRUE)
 
-      return(list(success = TRUE, tile = tile_id))
+      rm(df_cor, cor_r, pval_r)
+      gc()
 
     }, error = function(e) {
       msg <- sprintf("Tile %s failed: %s", args$tile_id %||% "unknown", conditionMessage(e))
@@ -85,11 +92,29 @@ gc()
 # Print processing time
 message(sprintf("done [%.1f min]", difftime(Sys.time(), t0, units = "mins")))
 
-# -------- Combination ----------------------------------------------------------
+# -------- Combination ---------------------------------------------------------
 
+
+# mosacing the r(H~TWI) map
 mosaic_tiles(
   input_dir   = cor_twi_vegh_tiles_dir,
   output_file = cor_twi_vegh_mosaic_file,
-  layer_names = c("correlation", "cor_pval")
-)
+  pattern = "*_map.nc",
+  varname = "correlation")
+
+# mosacing the pval ofr(H~TWI) map
+mosaic_tiles(
+  input_dir   = cor_twi_vegh_tiles_dir,
+  output_file = pval_cor_twi_vegh_mosaic_file,
+  pattern = "*_pval.nc",
+  varname = "cor_pval")
+
+
+# ---------- Delete intermediate data ------------------------------------------
+# List all files in the directory cor_twi_vegh_tiles_dir that match "*_to450m.nc"
+# If there are any files found, delete them
+
+# cor_5km_tiles_path <- fs::dir_ls(path = cor_twi_vegh_tiles_dir, glob = "*_to450m.nc")
+# if (length(cor_5km_tiles_path) > 0) file.remove(cor_5km_tiles_path)
+
 
