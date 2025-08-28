7eb68b635fc581b15f267e4b00fbd4107b4d576c
Ting Tan <tanting0502@163.com>
2025-08-28 13:23:18 +0200

    Crop raster before ggplot to avoid plotting very large data

--- R/plot_cor_pval.R
+++ R/plot_cor_pval.R
@@ -9,6 +9,7 @@
 #' @export
 plot_cor_pval <- function(input, extent = NULL, title_text = "VEGH–TWI Pearson Correlation: P-value Map", text_size = 16, x_breaks = 30, y_breaks = 30) {
 
+
   if (is.character(input)) {
     input <- terra::rast(input)
   }
@@ -21,6 +22,22 @@ plot_cor_pval <- function(input, extent = NULL, title_text = "VEGH–TWI Pearson
     extent <- terra::ext(input)
   } else if (!inherits(extent, "SpatExtent")) {
     stop("`extent` must be a SpatExtent object created by terra::ext().")
+  } else {
+
+    # crop the input raster if the plot area is smaller than the original raster
+    area_in <- (xmax(input) - xmin(input)) * (ymax(input) - ymin(input))
+    area_ex <- (xmax(extent) - xmin(extent)) * (ymax(extent) - ymin(extent))
+
+    if (area_ex < area_in) {
+      cropped <- terra::crop(input, extent)
+
+      if (all(is.na(values(cropped)))) {
+        stop("The specified extent does not intersect with the raster. No plot will be generated.")
+      } else {
+        input <- cropped
+        message("Raster has been cropped to the intersecting area of the extent.")
+      }
+    }
   }
 
   xmin <- extent$xmin
