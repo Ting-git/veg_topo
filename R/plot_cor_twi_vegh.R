417477c5cc62ebac69eea5753e9858d39224af27
Ting Tan <tanting0502@163.com>
2025-08-28 13:23:51 +0200

    Crop raster before ggplot to avoid plotting very large data Fix correlation limits to range from -1 to 1

--- R/plot_cor_twi_vegh.R
+++ R/plot_cor_twi_vegh.R
@@ -1,13 +1,16 @@
 #' Plot Correlation between VEGH and TWI
 #'
-#' @param input A SpatRaster or raster file path
-#' @param extent Optional terra::ext() extent
+#' @param input A SpatRaster object or a raster file path
+#' @param extent Optional terra::ext() extent.
+#'   If provided and smaller than the input raster extent,
+#'   the raster will be cropped to their intersecting area.
 #' @param text_size Font size
 #' @param x_breaks Number of x-axis breaks
 #' @param y_breaks Number of y-axis breaks
 #' @return A ggplot2 object
 #' @export
-plot_cor_twi_vegh <- function(input, extent = NULL, title_text = "r(H ~ TWI)", text_size = 6, x_breaks = 5, y_breaks = 5) {
+plot_cor_twi_vegh <- function(input, extent = NULL, title_text = "r (H ~ TWI)",
+                              text_size = 6, x_breaks = 5, y_breaks = 5) {
 
   if (is.character(input)) {
     input <- terra::rast(input)
@@ -21,6 +24,22 @@ plot_cor_twi_vegh <- function(input, extent = NULL, title_text = "r(H ~ TWI)", t
     extent <- terra::ext(input)
   } else if (!inherits(extent, "SpatExtent")) {
     stop("`extent` must be a SpatExtent object created by terra::ext().")
+  } else {
+
+    # crop the input raster if the plot area is smaller than the original raster
+    area_in <- (xmax(input) - xmin(input)) * (ymax(input) - ymin(input))
+    area_ex <- (xmax(extent) - xmin(extent)) * (ymax(extent) - ymin(extent))
+
+    if (area_ex < area_in) {
+      cropped <- terra::crop(input, extent)
+
+      if (all(is.na(values(cropped)))) {
+        stop("The specified extent does not intersect with the raster. No plot will be generated.")
+      } else {
+        input <- cropped
+        message("Raster has been cropped to the intersecting area of the extent.")
+      }
+    }
   }
 
   xmin <- extent$xmin
@@ -28,15 +47,12 @@ plot_cor_twi_vegh <- function(input, extent = NULL, title_text = "r(H ~ TWI)", t
   ymin <- extent$ymin
   ymax <- extent$ymax
 
-  # Estimate value range for diverging color scale
-  vmin <- terra::global(input, "min", na.rm = TRUE)[1, 1] |> as.numeric()
-  vmax <- terra::global(input, "max", na.rm = TRUE)[1, 1] |> as.numeric()
-
   p <- ggplot2::ggplot() +
     tidyterra::geom_spatraster(data = input, maxcell = Inf) +
     scico::scale_fill_scico(
       palette = "bam",
-      limits = c(vmin, vmax),
+      limits = c(-1, 1),
+      breaks = seq(-1, 1, by = 0.5),
       midpoint = 0,
       name = expression(r[H*","*TWI]),
       na.value = NA
