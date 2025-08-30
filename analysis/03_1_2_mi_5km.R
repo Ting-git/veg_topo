53de43be12b11e97ca3189d1cca493a426ab25f3
Ting Tan <tanting0502@163.com>
2025-08-30 20:45:32 +0200

    rename

--- analysis/03_1_2_mi_5km.R
+++ analysis/03_1_2_mi_5km.R
@@ -7,14 +7,15 @@ source(here::here("R/aggregate_byfile.R"))
 # ------ Aggregation -----------------------------------------------------------
 
 aggregate_byfile(
-  input_path = ai_950m_file,
-  output_path = ai_5km_file,
+  input_path = mi_950m_file,
+  output_path = mi_5km_file,
   target_path = cor_twi_vegh_mosaic_file,
+  varname = "moisture_index",
   if_resample = TRUE,
   na_value = 0
 )
 
 # check the output
-# r <- rast(ai_5km_file)
+# r <- rast(mi_5km_file)
 # plot(r)
 # summary(r)
