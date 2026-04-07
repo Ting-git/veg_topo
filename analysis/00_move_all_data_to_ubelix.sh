5ccb67c20b127023f0648110d2cfd47f20040940
Ting Tan <tanting0502@163.com>
2026-04-07 15:06:33 +0200

    Add fveg calculation in vegh tile aggregation

--- analysis/00_move_all_data_to_ubelix.sh
+++ analysis/00_move_all_data_to_ubelix.sh
@@ -3,130 +3,149 @@ set -e  # if error, stop
 
 echo "=== START DATA TRANSFER ==="
 
-# TWI 450m
-rsync -avhP --info=progress2 --no-perms --no-owner --no-group \
-  /data_2/scratch/ting/veg_topo_data/data/global_twi_450m_clean \
-  tt22k003@submit04.unibe.ch:/storage/scratch/giub_geco/tting/
-
-# flc 5km
-rsync -avhP --info=progress2 --no-perms --no-owner --no-group \
-  /data_2/scratch/ting/veg_topo_data/data/global_flc_5km \
-  tt22k003@submit04.unibe.ch:/storage/scratch/giub_geco/tting/
-
-# r_H_TWI 5km
-rsync -avhP --info=progress2 --no-perms --no-owner --no-group \
-  /data_2/scratch/ting/veg_topo_data/data/global_cor_twi_vegh \
-  tt22k003@submit04.unibe.ch:/storage/scratch/giub_geco/tting/
-
-# r_H_Rin 5km
-rsync -avhP --info=progress2 --no-perms --no-owner --no-group \
-  /data_2/scratch/ting/veg_topo_data/data/global_r_H_R_5km \
-  tt22k003@submit04.unibe.ch:/storage/scratch/giub_geco/tting/
-
-# k-means 5km
-rsync -avhP --info=progress2 --no-perms --no-owner --no-group \
-  /data_2/scratch/ting/veg_topo_data/data/global_kmeans_5km \
-  tt22k003@submit04.unibe.ch:/storage/scratch/giub_geco/tting/
-
-# mi 5km
-rsync -avhP --info=progress2 --no-perms --no-owner --no-group \
-  /data_2/scratch/ting/veg_topo_data/data/global_mi_5km \
-  tt22k003@submit04.unibe.ch:/storage/scratch/giub_geco/tting/
-
-# mat 5km
-rsync -avhP --info=progress2 --no-perms --no-owner --no-group \
-  /data_2/scratch/ting/veg_topo_data/data/global_mat_5km \
-  tt22k003@submit04.unibe.ch:/storage/scratch/giub_geco/tting/
-
-# VEGH 450m
-rsync --human-readable -i --info=progress2 -avhP --no-perms --no-owner --no-group \
-  /data_2/scratch/ting/veg_topo_data/data/global_vegh_450m/vegh_450m_2020_mosaic.nc \
-  tt22k003@submit04.unibe.ch:/storage/scratch/giub_geco/tting/global_vegh_450m/
-
-# reg_sample
-rsync -avhP --info=progress2 --no-perms --no-owner --no-group \
-  --chmod=Du+rwx,Dgo+rx,Fu+rw,Fgo+r \
-  /data_2/scratch/ting/veg_topo_data/data/reg_sample \
-  tt22k003@submit04.unibe.ch:/storage/scratch/giub_geco/tting/
-
-# global_dem_slope_aspect_450m
-rsync -avhP --info=progress2 --no-perms --no-owner --no-group \
-  --chmod=Du+rwx,Dgo+rx,Fu+rw,Fgo+r \
-  /data_2/scratch/ting/veg_topo_data/data/global_dem_slope_aspect_450m \
-  tt22k003@submit04.unibe.ch:/storage/scratch/giub_geco/tting/
-
-# ecoregion_5km
-rsync --human-readable -i --info=progress2 -avhP --no-owner --no-group \
-  /data_2/scratch/ting/veg_topo_data/data/global_ecoregion_5km \
-  tt22k003@submit04.unibe.ch:/storage/scratch/giub_geco/tting/
-
-# TWI 30m
-rsync -avhP --info=progress2 --no-owner --no-group \
-  --chmod=Du+rwx,Dgo+rx,Fu+rw,Fgo+r \
-  /data_2/archive/twi_ho_2025 \
-  tt22k003@submit04.unibe.ch:/storage/scratch/giub_geco/tting/
-
-# Koeppen-Geiger
-rsync --human-readable -i --info=progress2 -avhP \
-  --chmod=Du+rwx,Dgo+rx,Fu+rw,Fgo+r \
-  --no-owner --no-group \
-  /data/archive/koeppengeiger_beck_2018/ \
-  tt22k003@submit04.unibe.ch:/storage/scratch/giub_geco/tting/koeppengeiger_beck_2018/
-
-# cci 300m
-rsync --human-readable -i --info=progress2 -avhP \
-  --chmod=Du+rwx,Dgo+rx,Fu+rw,Fgo+r \
-  --no-owner --no-group \
-  /data/archive/landcover_defourny_2023 \
-  tt22k003@submit04.unibe.ch:/storage/scratch/giub_geco/tting/
-
+# ========= Raw data ===========
 # VEGH 10m
 rsync --human-readable -i --info=progress2 -avhP \
   --chmod=Du+rwx,Dgo+rx,Fu+rw,Fgo+r \
   --no-owner --no-group \
   /data_2/archive/vegheight_lang_2023 \
-  tt22k003@submit04.unibe.ch:/storage/scratch/giub_geco/tting/
+  tt22k003@submit04.unibe.ch:/storage/scratch/giub_geco/tting/data_raw/
 
 # DEM 30m
 rsync --human-readable -i --info=progress2 -avhP --no-owner --no-group \
-  /data_2/scratch/ting/veg_topo_data/data_raw/copernicus_dem_30m/ \
-  tt22k003@submit04.unibe.ch:/storage/scratch/giub_geco/tting/copernicus_dem_30m/
+  /data_2/scratch/ting/veg_topo_data/data_raw/copernicus_dem_30m \
+  tt22k003@submit04.unibe.ch:/storage/scratch/giub_geco/tting/data_raw/
+
+# TWI 30m -- Ho et al. 2025
+rsync -avhP --info=progress2 --no-owner --no-group \
+  --chmod=Du+rwx,Dgo+rx,Fu+rw,Fgo+r \
+  /data_2/archive/twi_ho_2025 \
+  tt22k003@submit04.unibe.ch:/storage/scratch/giub_geco/tting/data_raw/
+
+# TWI 450m -- Marthew et. al 2015
+rsync -avhP --info=progress2 --no-owner --no-group \
+  --chmod=Du+rwx,Dgo+rx,Fu+rw,Fgo+r \
+  /data/archive/gti_marthews_2015 \
+  tt22k003@submit04.unibe.ch:/storage/scratch/giub_geco/tting/data_raw/
+
+# MI 950m -- Zomer 2022
+rsync -avhP --info=progress2 --no-owner --no-group \
+  --chmod=Du+rwx,Dgo+rx,Fu+rw,Fgo+r \
+  /data/archive/aridityindex_zomer_2022 \
+  tt22k003@submit04.unibe.ch:/storage/scratch/giub_geco/tting/data_raw/
+
+# Climatic Variables
+rsync -avhP --info=progress2 --no-owner --no-group \
+  --chmod=Du+rwx,Dgo+rx,Fu+rw,Fgo+r \
+  /data/archive/worldclim_fick_2017 \
+  tt22k003@submit04.unibe.ch:/storage/scratch/giub_geco/tting/data_raw/
+
+# Koeppen-Geiger
+rsync --human-readable -i --info=progress2 -avhP \
+  --chmod=Du+rwx,Dgo+rx,Fu+rw,Fgo+r \
+  --no-owner --no-group \
+  /data/archive/koeppengeiger_beck_2018 \
+  tt22k003@submit04.unibe.ch:/storage/scratch/giub_geco/tting/data_raw/
+
+# CCI land cover 300m
+rsync --human-readable -i --info=progress2 -avhP \
+  --chmod=Du+rwx,Dgo+rx,Fu+rw,Fgo+r \
+  --no-owner --no-group \
+  /data/archive/landcover_defourny_2023 \
+  tt22k003@submit04.unibe.ch:/storage/scratch/giub_geco/tting/data_raw/
 
 # WDPA: world database on Protected area
 rsync --human-readable -i --info=progress2 -avhP --no-owner --no-group \
   /data_2/scratch/ting/veg_topo_data/data_raw/wdpa_2025 \
-  tt22k003@submit04.unibe.ch:/storage/scratch/giub_geco/tting/
+  tt22k003@submit04.unibe.ch:/storage/scratch/giub_geco/tting/data_raw/
 
+# ========= Processed data ===========
 
+# TWI (clean) 450m
+rsync -avhP --info=progress2 --no-perms --no-owner --no-group \
+  /data_2/scratch/ting/veg_topo_data/data/global_twi_450m_clean \
+  tt22k003@submit04.unibe.ch:/storage/scratch/giub_geco/tting/data/
+
+# flc 5km
+rsync -avhP --info=progress2 --no-perms --no-owner --no-group \
+  /data_2/scratch/ting/veg_topo_data/data/global_flc_5km \
+  tt22k003@submit04.unibe.ch:/storage/scratch/giub_geco/tting/data/
+
+# r_H_TWI 5km
+rsync -avhP --info=progress2 --no-perms --no-owner --no-group \
+  /data_2/scratch/ting/veg_topo_data/data/global_cor_twi_vegh \
+  tt22k003@submit04.unibe.ch:/storage/scratch/giub_geco/tting/data/
+
+# r_H_Rin 5km
+rsync -avhP --info=progress2 --no-perms --no-owner --no-group \
+  /data_2/scratch/ting/veg_topo_data/data/global_r_H_R_5km \
+  tt22k003@submit04.unibe.ch:/storage/scratch/giub_geco/tting/data/
+
+# k-means 5km
+rsync -avhP --info=progress2 --no-perms --no-owner --no-group \
+  /data_2/scratch/ting/veg_topo_data/data/global_kmeans_5km \
+  tt22k003@submit04.unibe.ch:/storage/scratch/giub_geco/tting/data/
+
+# mi 5km
+rsync -avhP --info=progress2 --no-perms --no-owner --no-group \
+  /data_2/scratch/ting/veg_topo_data/data/global_mi_5km \
+  tt22k003@submit04.unibe.ch:/storage/scratch/giub_geco/tting/data/
+
+# mat 5km
+rsync -avhP --info=progress2 --no-perms --no-owner --no-group \
+  /data_2/scratch/ting/veg_topo_data/data/global_mat_5km \
+  tt22k003@submit04.unibe.ch:/storage/scratch/giub_geco/tting/data/
+
+# VEGH 450m
+rsync --human-readable -i --info=progress2 -avhP --no-perms --no-owner --no-group \
+  /data_2/scratch/ting/veg_topo_data/data/global_vegh_450m \
+  tt22k003@submit04.unibe.ch:/storage/scratch/giub_geco/tting/data/
+
+# reg_sample
+rsync -avhP --info=progress2 --no-perms --no-owner --no-group \
+  --chmod=Du+rwx,Dgo+rx,Fu+rw,Fgo+r \
+  /data_2/scratch/ting/veg_topo_data/data/reg_sample \
+  tt22k003@submit04.unibe.ch:/storage/scratch/giub_geco/tting/data/
+
+# global_dem_slope_aspect_450m
+rsync -avhP --info=progress2 --no-perms --no-owner --no-group \
+  --chmod=Du+rwx,Dgo+rx,Fu+rw,Fgo+r \
+  /data_2/scratch/ting/veg_topo_data/data/global_dem_slope_aspect_450m \
+  tt22k003@submit04.unibe.ch:/storage/scratch/giub_geco/tting/data/
+
+# ecoregion_5km
+rsync --human-readable -i --info=progress2 -avhP --no-owner --no-group \
+  /data_2/scratch/ting/veg_topo_data/data/global_ecoregion_5km \
+  tt22k003@submit04.unibe.ch:/storage/scratch/giub_geco/tting/data/
 
 # dem 55km: dem_sd_55km, dem_rg_55km
 rsync -avhP --info=progress2 --no-perms --no-owner --no-group \
   /data_2/scratch/ting/veg_topo_data/data/global_dem_55km \
-  tt22k003@submit04.unibe.ch:/storage/scratch/giub_geco/tting/
+  tt22k003@submit04.unibe.ch:/storage/scratch/giub_geco/tting/data/
 
 # flc 55km
 rsync -avhP --info=progress2 --no-perms --no-owner --no-group \
   /data_2/scratch/ting/veg_topo_data/data/global_flc_55km \
-  tt22k003@submit04.unibe.ch:/storage/scratch/giub_geco/tting/
+  tt22k003@submit04.unibe.ch:/storage/scratch/giub_geco/tting/data/
 
 # mi 55km
 rsync -avhP --info=progress2 --no-perms --no-owner --no-group \
   /data_2/scratch/ting/veg_topo_data/data/global_mi_55km \
-  tt22k003@submit04.unibe.ch:/storage/scratch/giub_geco/tting/
+  tt22k003@submit04.unibe.ch:/storage/scratch/giub_geco/tting/data/
 
 # fpa 55km
 rsync -avhP --info=progress2 --no-perms --no-owner --no-group \
   /data_2/scratch/ting/veg_topo_data/data/global_fpa_55km_2025 \
-  tt22k003@submit04.unibe.ch:/storage/scratch/giub_geco/tting/
+  tt22k003@submit04.unibe.ch:/storage/scratch/giub_geco/tting/data/
 
 # fveg 55km
 rsync -avhP --info=progress2 --no-perms --no-owner --no-group \
   /data_2/scratch/ting/veg_topo_data/data/global_fveg_55km \
-  tt22k003@submit04.unibe.ch:/storage/scratch/giub_geco/tting/
+  tt22k003@submit04.unibe.ch:/storage/scratch/giub_geco/tting/data/
 
 # mat 55km
 rsync -avhP --info=progress2 --no-perms --no-owner --no-group \
   /data_2/scratch/ting/veg_topo_data/data/global_mat_55km \
-  tt22k003@submit04.unibe.ch:/storage/scratch/giub_geco/tting/
+  tt22k003@submit04.unibe.ch:/storage/scratch/giub_geco/tting/data/
 
 echo "=== ALL TRANSFERS COMPLETED ==="
--- analysis/1_02_1_vegh_450m_agg_bytile.R
+++ analysis/1_02_1_vegh_450m_agg_bytile.R
@@ -7,97 +7,144 @@ library(terra)
 library(furrr)
 library(future)
 
-# Automatically select configuration file
+source(here::here("R/config.R"))
+source(here::here("R/raster_preprocess_save.R")) # Set Na value and aggregation
+
+# Set worker numbers for different system
 hostname <- trimws(tolower(system("hostname", intern = TRUE)))
 if (hostname == "dash") {
-  message("💻 Detected Worksation: dash → using config.R")
-  source(here::here("config.R"))
   workers = 4
+  message("→ using ", workers, " workers")
+
 } else {
-  message("🖥️ Detected HPC environment (", hostname, ") → using config_ubelix.R")
-  source(here::here("config_ubelix.R"))
   workers = 16
+  message("→ using ", workers, " workers")
 }
-source(here::here("R/raster_preprocess_save.R")) # Set Na value and aggregation
 
 # -------Configuration----------------------------------------------------------
 
 # Input: read all 10m vegetation data paths (2651)
 vegh_10m_tiles_path <- fs::dir_ls(path = vegh_10m_tiles_dir, glob = "*_Map.tif")
-vegh_10m_tiles_path_sub <- vegh_10m_tiles_path[1:2651]  # for test
+vegh_10m_tiles_path_sub <- vegh_10m_tiles_path[1:1]  # subset for testing (limit number of tiles)!!!!!!
 
 # Output: 2651 tiles with 450m resolution
 if (!dir.exists(vegh_450m_tiles_dir)) {
   dir.create(vegh_450m_tiles_dir, recursive = TRUE)
 }
 
-# -----Aggregation: 10m -> 450m ------------------------------------------------
-
 # target layer
 twi_450m_r <- terra::rast(twi_450m_mosaic_clean_path)
 res_tar <- res(twi_450m_r)
 rm(twi_450m_r ); gc()
 
-# Set up parallel processing (adjust based on available CPU cores)
-plan(multisession, workers = workers)
-t0 <- Sys.time()
+# -----Aggregation: 10m -> 450m ------------------------------------------------
 
-# Parallel processing of raster files with error handling
+agg_vegh_fveg <- function(vegh_10m_file) {
+
+  tryCatch({
+
+    # --- tile info ---
+    tile_id <- basename(vegh_10m_file)
+    tictoc::tic(paste0("Processing tile: ", tile_id))
+    t0 <- Sys.time()
+
+    # set output file
+    vegh_450m_file <- file.path(
+      vegh_450m_tiles_dir,
+      paste0(sub("\\.tif$", "", basename(vegh_10m_file)), "_to450m.nc")
+    )
+
+    # set output file
+    fveg_450m_file <- file.path(
+      vegh_450m_tiles_dir,
+      paste0(sub("\\.tif$", "", basename(vegh_10m_file)), "_to450m_fveg.nc")
+    )
+
+    # Check if files have been processed
+    if (fs::file_exists(vegh_450m_file) && fs::file_size(vegh_450m_file) > 0 && fs::file_exists(fveg_450m_file) && fs::file_size(fveg_450m_file) > 0) {
+      return(TRUE)
+    }
+
+    # --- aggregate vegetation height ---
+    # Set 0 as NA value (0m canopy height represents not vegetated or water according to Lang et al. (2019))
+    # Aggregates using TWI data from Marthews et al. (2015) as target resolution
+    # Return the saved path
+    agg_vegh <- raster_preprocess_save(
+      input = vegh_10m_file,
+      output = vegh_450m_file,
+      res_tar = res_tar,
+      na_value = 0,
+      fun = mean,
+      varname = "vegh",
+      if_aggregate = TRUE,
+      if_round_fact = TRUE,
+      if_resample = FALSE,
+      if_mask = FALSE,
+      if_return_raster = FALSE
+    )
+
+    # --- Aggregate fraction of vegetated area ---
+    # Compute the fraction of pixels with vegetation (height > 0)
+    # while treating 0 and NA as non-vegetated.
+    # Aggregation is done at the target resolution defined by TWI data (Marthews et al., 2015).
+    r_out <- raster_preprocess_save(
+      input        = vegh_10m_file,
+      output       = fveg_450m_file,
+      res_tar      = res_tar,
+      varname      = "fveg",
+      if_aggregate = TRUE,
+      fun = function(x, na.rm) {
+        total <- length(x)                  # total number of pixels INCLUDING NA
+        if (all(is.na(x))) return(NA)       # if all NA return NA
+        veg_count <- sum(!is.na(x) & x > 0)  # count of vegetated pixels
+        return(veg_count / total)            # fraction over TOTAL pixels, including NA
+      },
+      if_round_fact = TRUE,
+      if_resample = FALSE,
+      if_mask = FALSE,
+      if_return_raster = FALSE
+    )
+
+    # --- Print proccess time ---
+    elapsed_mins <- difftime(Sys.time(), t0, units = "mins")
+    message(sprintf("Tile %s completed [%.1f mins]", tile_id, elapsed_mins))
+    tictoc::toc()
+
+    return(TRUE)
+
+  }, error = function(e) {
+    elapsed_mins <- difftime(Sys.time(), t0, units = "mins")
+    message(sprintf("❌ Tile %s failed after %.1f mins: %s", basename(vegh_10m_file), elapsed_mins, e$message))
+    return(FALSE)
+  })
+}
+
+# ----------------- Parallel execution for all tiles-----------------
+# Set up cluster plan
+plan(cluster, workers = workers)
+
+# Run in parallel
+tictoc::tic("🚀 Parallel processing of tiles")
+
 results <- future_map(
-
-  vegh_10m_tiles_path_sub, # for test
-  # vegh_10m_tiles_path,  # for all
-
-  function(file) {
-    tryCatch({
-
-      # Set output file
-      output <- file.path(
-        vegh_450m_tiles_dir,
-        paste0(sub("\\.tif$", "", basename(file)), "_to450m.nc")
-      )
-
-      # # Check if it has been processed
-      # if (fs::file_exists(output) && fs::file_size(output) > 0) {
-      #   return(list(success = TRUE, out_file = output, error = NULL))
-      # }
-
-
-      # Set 0 as NA value (0m canopy height represents not vegetated or water according to Lang et al. (2019))
-      # Aggregates using TWI data from Marthews et al. (2015) as target resolution
-      # Return the saved path
-      result <- raster_preprocess_save(
-        input = file,
-        output = output,
-        res_tar = res_tar,
-        na_value = 0,
-        fun = mean,
-        varname = "vegh",
-        if_aggregate = TRUE,
-        if_round_fact = TRUE,
-        if_resample = FALSE,
-        if_mask = FALSE,
-        if_return_raster = FALSE
-      )
-
-      list(success = TRUE, out_file = result, error = NULL)
-    }, error = function(e) {
-      message(sprintf("❌ Error processing file: %s", file))
-      message(sprintf("  → %s", e$message))
-      list(success = FALSE, out_file = NULL, error = e$message)
-    }, finally = { gc() })
-
-  },
+  vegh_10m_tiles_path_sub,
+  agg_vegh_fveg,
   .progress = FALSE,
-  .options = furrr::furrr_options(seed = FALSE,
-                                  globals = c("res_tar", "raster_preprocess_save", "vegh_450m_tiles_dir"),
-                                  packages = c("terra", "fs"))
+  .options = furrr_options(seed=TRUE)
 )
 
 plan(sequential)
-gc()
+tictoc::toc()
 
-# print precess duration
-message(sprintf("done [%.1fs]", difftime(Sys.time(), t0, units = "secs")))
-saveRDS(results, file = here::here("data/results_vegh_aggregate.rds"))
+# Summarize results
+success_count <- sum(unlist(results))
+fail_count <- length(results) - success_count
+message(sprintf("✅ Completed: %d succeeded, ❌ %d failed.", success_count, fail_count))
 
+# ------ Single tile check (optional) ---------------------------------------
+# r0 <- rast(file.path(vegh_10m_tiles_dir,"ETH_GlobalCanopyHeight_10m_2020_N00E006_Map.tif"))
+# r1 <- rast("/data_2/scratch/ting/veg_topo_data/data/global_vegh_450m/..._to450m.nc")
+# r2 <- rast("/data_2/scratch/ting/veg_topo_data/data/global_vegh_450m/..._to450m_fveg.nc")
+# plot(r0, xlim=c(6.6,6.7), ylim=c(0.1,0.2))
+# plot(r1, xlim=c(6.6,6.7), ylim=c(0.1,0.2))
+# plot(r2, xlim=c(6.6,6.7), ylim=c(0.1,0.2))
