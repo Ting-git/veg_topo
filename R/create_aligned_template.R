6389e25762b08bf203dde52bd6091ea9f7efc94d
Ting Tan <tanting0502@163.com>
2026-04-12 18:55:44 +0200

    fix alignment logic in create_aligned_template()

--- R/create_aligned_template.R
+++ R/create_aligned_template.R
@@ -15,18 +15,34 @@
 #' template
 create_aligned_template <- function(input, dwin = 0.05, crs_out = NULL) {
 
+  # ---- Helper function to determine decimal places ----
+  get_decimal_places <- function(x) {
+    if (x == 0) return(0)
+    x_str <- format(x, scientific = FALSE)
+    if (!grepl("\\.", x_str)) return(0)
+    nchar(strsplit(x_str, "\\.")[[1]][2])
+  }
+
+  # Automatically determine rounding precision from dwin
+  dwin_decimals <- get_decimal_places(dwin)
+  round <- dwin_decimals + 1
+
   # ---- Load raster ----
   if (is.character(input)) input <- terra::rast(input)
   if (!inherits(input, "SpatRaster")) stop("Input must be a SpatRaster or valid file path.")
 
   # Get input raster extent
   e <- ext(input)
+  xmin <- round(e$xmin, round)
+  xmax <- round(e$xmax, round)
+  ymin <- round(e$ymin, round)
+  ymax <- round(e$ymax, round)
 
   # Align the extent to the grid defined by dwin (floor/ceiling)
-  xmin_aligned <- floor(e$xmin / dwin) * dwin
-  xmax_aligned <- ceiling(e$xmax / dwin) * dwin
-  ymin_aligned <- floor(e$ymin / dwin) * dwin
-  ymax_aligned <- ceiling(e$ymax / dwin) * dwin
+  xmin_aligned <- floor(xmin / dwin) * dwin
+  xmax_aligned <- ceiling(xmax / dwin) * dwin
+  ymin_aligned <- floor(ymin / dwin) * dwin
+  ymax_aligned <- ceiling(ymax / dwin) * dwin
 
   # Compute number of rows and columns to cover the aligned extent
   ncols <- round((xmax_aligned - xmin_aligned) / dwin)
